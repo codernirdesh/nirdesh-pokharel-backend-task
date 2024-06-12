c70788c20795930511a3b4d5beb258dc29dbfcf9
Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>
2024-06-12 15:23:34 +0545

    chore: Update constants/index.ts to include API_V1_PREFIX, JWT_SECRET, and JWT_EXPIRES_IN
    Signed-off-by: Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,8 @@
-export const APP_PORT: number = parseInt(process.env.PORT ?? "3000", 10);
+import { loadEnvFile } from "node:process";
+
+loadEnvFile();
+
+export const APP_PORT = parseInt(process.env.PORT ?? "3000", 10);
+export const API_V1_PREFIX = "/api/v1";
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
