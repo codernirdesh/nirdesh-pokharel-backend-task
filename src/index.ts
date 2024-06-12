d412b8eca42a2c2ad663d7c9f2338bbb4e2f5def
Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>
2024-06-12 12:30:40 +0545

    refactor: moved index.ts inside src
    Signed-off-by: Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
-import { APP_PORT } from "./src/constants";
+import { APP_PORT } from "./constants";
+import UserRoute from "./routes/user.route";
 const app = express();
 
 // Using expresses built-in middleware to parse incoming requests with JSON payloads
@@ -9,6 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello World!");
 });
+
+// Routes
+app.use(UserRoute);
 
 app.listen(APP_PORT, () => {
 	console.log(`Server is running on port ${APP_PORT}`);
