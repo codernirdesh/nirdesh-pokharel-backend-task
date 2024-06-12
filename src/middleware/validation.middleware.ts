7c0a7e2efbb8ec671f5104ec6691da705cb9f4e3
Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>
2024-06-12 15:21:56 +0545

    chore: Add validation middleware for request body
    Signed-off-by: Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>

---
+++ src/middleware/validation.middleware.ts
@@ -0,0 +1,24 @@
+import { plainToInstance } from "class-transformer";
+import { validate, ValidationError } from "class-validator";
+import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../helpers/error.helper";
+import { StatusCodes } from "http-status-codes";
+
+export function validationMiddleware<T>(
+	type: any
+): (req: Request, res: Response, next: NextFunction) => void {
+	return (req: Request, res: Response, next: NextFunction) => {
+		const dto = plainToInstance(type, req.body);
+		validate(dto).then((errors: ValidationError[]) => {
+			if (errors.length > 0) {
+				const message = errors
+					.map((error) => Object.values(error.constraints || {}).join(", "))
+					.join(", ");
+				throw CustomError(StatusCodes.BAD_REQUEST, "Validation Error", message);
+			} else {
+				req.body = dto as T;
+				next();
+			}
+		});
+	};
+}
