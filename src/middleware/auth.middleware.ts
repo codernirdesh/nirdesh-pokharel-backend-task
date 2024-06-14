fb40da1df4bd8a1da0368e1801a3fcc153ef8b53
Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>
2024-06-14 11:52:37 +0545

    refactor: Add authenticatedUI and roleUi middleware functions
    Signed-off-by: Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -26,3 +26,26 @@ export function authenticated(
 	req.user = decoded;
 	next(); // Call next middleware
 }
+
+export function authenticatedUI(
+	req: RequestWithUser,
+	res: Response,
+	next: NextFunction
+) {
+	const bearerToken = req.header("authorization")?.split(" ")[1];
+	const cookieToken = req.cookies.token;
+	const token = bearerToken || cookieToken;
+
+	if (!token) {
+		return res.redirect("/login");
+	}
+
+	// Verify token using JWtHelper
+	const decoded = JWTHelper.verifyToken(token);
+	if (!decoded) {
+		return res.redirect("/login");
+	}
+
+	req.user = decoded;
+	next(); // Call next middleware
+}
